2ea361a2daab102b6969026caedef6e940c036d9
acristoni <adonai_cn@hotmail.com>
2023-09-01 15:33:14 -0300

    FEAT-BACK: DTOs

--- back-cogna-cadastro-cliente/src/client/dto/create-client.dto.ts
+++ back-cogna-cadastro-cliente/src/client/dto/create-client.dto.ts
@@ -1,1 +1,31 @@
-export class CreateClientDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { EstadoCivil } from '../enums/estadocivil.enum';
+
+export class CreateClientDto {
+    @ApiProperty({
+        description: 'Nome do cliente',
+        example: 'Valdecir',
+    })
+    nome: string;
+
+    @ApiProperty({
+        description: 'Data de nascimento do cliente',
+        example: '1990-01-01',
+    })
+    dataNascimento: string;
+
+    @ApiProperty({
+        description: 'CPF do cliente',
+        example: '49317131069',
+    })
+    cpf: string;
+
+    @ApiProperty({
+        type: 'enum',
+        enum: EstadoCivil,
+        default: EstadoCivil.SOLTEIRO,
+        example: EstadoCivil.SOLTEIRO,
+        description: 'Estado civil do usuário',
+      })
+      estadoCivil: EstadoCivil;
+}
--- back-cogna-cadastro-cliente/src/client/dto/update-client.dto.ts
+++ back-cogna-cadastro-cliente/src/client/dto/update-client.dto.ts
@@ -1,4 +1,35 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateClientDto } from './create-client.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { EstadoCivil } from '../enums/estadocivil.enum';
 
-export class UpdateClientDto extends PartialType(CreateClientDto) {}
+export class UpdateClientDto {
+    @ApiProperty({
+        description: 'Nome do cliente',
+        example: 'Valdecir',
+        required: false
+    })
+    nome?: string;
+
+    @ApiProperty({
+        description: 'Data de nascimento do cliente',
+        example: '1990-01-01',
+        required: false,
+    })
+    dataNascimento?: string;
+
+    @ApiProperty({
+        description: 'CPF do cliente',
+        example: '49317131069',
+        required: false
+    })
+    cpf?: string;
+
+    @ApiProperty({
+        type: 'enum',
+        enum: EstadoCivil,
+        default: EstadoCivil.SOLTEIRO,
+        example: EstadoCivil.SOLTEIRO,
+        description: 'Estado civil do usuário',
+        required: false
+    })
+    estadoCivil?: EstadoCivil;
+}
